27c61e59c8636ba90acf1b30a4527aa037694dd9
Pavel Gusev <farland271@gmail.com>
2026-06-21 13:19:50 +0300

    Init commit

--- src/scripts/Gamestate.cs
+++ src/scripts/Gamestate.cs
@@ -1,15 +1,69 @@
 using Godot;
 using System;
+public enum GameMode
+{
+	MainMenu,
+	MainGame,
+	EditMode
+} 
 
 public partial class Gamestate : Node
 {
+	[Export] public float generatorTorque;
+	[Export] public float generatorCooldown;
+	[Export] public float maxCharge;
+	[Export] public GameMode currentGamemode;
+	public float _baseSizeX;
+	public bool isPaused;
+	public float _baseSizeY;
+	public delegate void SwitchChargeHandler(bool _state);
+	private Player _playerInstance;
+	public Player playerInstance() => _playerInstance;
+	public SwitchChargeHandler switchCharge;
+	public delegate void SwitchGamemodeHandler(GameMode _gamemode);
+	public SwitchGamemodeHandler SwitchGamemode;
+	private float _currentCharge;
+	private bool _isOn;
+	public bool isOn()=> _isOn;
+	//Required for UI
+	public float currentCharge() => _currentCharge;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		this._currentCharge = maxCharge;
+		SwitchGamemode += SelectGamemode;
+		SwitchGamemode(this.currentGamemode);
+	}
+	public void Init(Player _player)
+	{
+		this._playerInstance = _player;
+		switchCharge += SwitchChargeVoid;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    private void SwitchChargeVoid(bool _state)
+    {
+        _isOn = _state;
+    }
+
+
+    private void SelectGamemode(GameMode _currentGamemode)
+    {
+        this.currentGamemode = _currentGamemode;
+		switch(this.currentGamemode)
+		{
+			case(GameMode.EditMode):
+				isPaused = true;
+				break;
+			default:
+				isPaused = false;
+			break;
+		}
+		SwitchGamemode(this.currentGamemode);
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
+		
 	}
 }
---
+++ src/scripts/Generator.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class Generator : Node
+{
+	public Gamestate gameState;
+	private float _spinCooldown;
+	private MeshInstance3D _turbine;
+	private bool _on;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		gameState.switchCharge += launchCharge;
+		_turbine = this.GetChild<MeshInstance3D>(1);
+	}
+
+	private void launchCharge(bool _state)
+	{
+	   _on = _state;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if(!_on)
+		{
+			if(_spinCooldown>0.0f)
+			{
+				_spinCooldown -= (float)delta;	
+			}			
+		}
+		else
+			_spinCooldown = gameState.generatorCooldown;
+		if(_turbine!=null)
+			_turbine.RotateZ(((float)delta*gameState.generatorTorque)*(_spinCooldown/gameState.generatorCooldown));
+	}
+}
